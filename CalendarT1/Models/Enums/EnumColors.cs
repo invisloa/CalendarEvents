3a9ef6b2bf2d84020452c11cacccea6098fd43fa
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-25 23:03:16 +0200

    making view

---
+++ CalendarT1/Models/Enums/EnumColors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarT1.Models.Enums
+{
+	//make enum class with 5 main colors
+
+
+	public enum EnumColors
+	{
+		Red,
+		Blue,
+		Green,
+		Yellow,
+		Orange
+	}
+
+
+}
--- CalendarT1/ViewModels/ScheduleListViewModel.cs
+++ CalendarT1/ViewModels/ScheduleListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CalendarT1.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,23 +9,52 @@ using System.Threading.Tasks;
 
 namespace CalendarT1.ViewModels
 {
-	public class ScheduleListViewModel
+	public partial class ScheduleListViewModel : ObservableObject
 	{
 		public ObservableCollection<ScheduleModel> ScheduleList { get; set; } = new ObservableCollection<ScheduleModel>();
 		private List<ScheduleModel> _allScheduleList = new List<ScheduleModel>();
 
+		[ObservableProperty]
+		private DateTime _currentDate = DateTime.Now;
 
-		public ScheduleListViewModel() 
+
+		public ScheduleListViewModel()
 		{
 			AddAllScheduleList();
-			
+
 		}
 
 		private void AddAllScheduleList()
 		{
-			var scheduleList = new ObservableCollection<ScheduleModel>();
-			scheduleList.Add(new ScheduleModel() { })
+			var scheduleList = new List<ScheduleModel>();
+			// add to scheduleList some dummy data for testing
+			//make background color randomized
+			for (int i = 0; i < 5; i++)
+			{
+				scheduleList.Add(new ScheduleModel
+				{
+					StartDateTime = DateTime.Now.AddDays(i),
+					EndDateTime = DateTime.Now.AddDays(i).AddHours(1),
+					Title = $"Test {i + 1}",
+					Description = $"Test {i + 1} Description",
+					BackgroundColor = Color.FromRgba(255, 0, 0, 0.5)
+				});
+			}
+			_allScheduleList.AddRange(scheduleList);
 
+			BindDataToScheduleList();
+		}
+
+		private void BindDataToScheduleList()
+		{
+			var filteredScheduleList = _allScheduleList.Where(x => x.StartDateTime.Date == CurrentDate.Date).ToList();
+
+			ScheduleList.Clear();
+			foreach (var item in filteredScheduleList)
+			{
+				ScheduleList.Add(item);
+			}
 		}
 	}
+
 }
