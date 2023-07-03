1df9615acbea85bb2dbe4b7071af875af418dc46
invisloa <91217120+invisloa@users.noreply.github.com>
2023-07-04 00:00:17 +0200

    todo plans

--- CalendarT1/TODO.cs
+++ CalendarT1/TODO.cs
@@ -1,32 +1,40 @@
 ﻿
-// make current week view, current month view, current day view
-// button to add new event -  create a new page for adding events
 
+
+
+
+#region Current
 // Window For event details?? click on event command?
 // CHANGE 		private void ExecuteSelectEventCommand(EventModel selectedEvent) TO show a new window with event details
+#endregion
 
 
 
-// if event datetime end is lower than start change it to start + 1 hour
 
+
+
+
+
+
+
+
+
+#region for later
+// make current week view, current month view, current day view
 // change priority level picker to button like choosing  <Picker Title="Priority Level" x: Name = "PriorityPicker" ItemsSource = "{Binding EventPriorities}" SelectedItem = "{Binding EventPriority, Mode=TwoWay}" >
 
+#endregion
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+#region done
 
 // account wide properties to show events
 // save and load interfaces
+// button to add new event -  create a new page for adding events
 
 // make a checkboxes for different events to show
+// if event datetime end is lower than start change it to start + 1 hour
+
+#endregion
+
