8121a0eb59802526acdbb7cadf3e4588e01cc81e
invisloa <91217120+invisloa@users.noreply.github.com>
2023-07-10 00:04:20 +0200

    working read write _phone

--- CalendarT1/App.xaml.cs
+++ CalendarT1/App.xaml.cs
@@ -8,4 +8,23 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+	protected override async void OnStart()
+	{
+		// Call base method
+		base.OnStart();
+
+		// Check or request StorageRead permission
+		var statusStorageRead = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+		if (statusStorageRead != PermissionStatus.Granted)
+		{
+			statusStorageRead = await Permissions.RequestAsync<Permissions.StorageRead>();
+		}
+		var statusStorageWrite = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+		if (statusStorageWrite != PermissionStatus.Granted)
+		{
+			statusStorageWrite = await Permissions.RequestAsync<Permissions.StorageRead>();
+		}
+
+
+	}
 }
