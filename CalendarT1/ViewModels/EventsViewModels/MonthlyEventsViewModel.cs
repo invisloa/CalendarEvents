9b93e0b30d773d2bd12bd7d789985429727f7efd
invisloa <91217120+invisloa@users.noreply.github.com>
2023-07-17 20:39:54 +0200

    ...

--- CalendarT1/ViewModels/EventsViewModels/MonthlyEventsViewModel.cs
+++ CalendarT1/ViewModels/EventsViewModels/MonthlyEventsViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CalendarT1.Models;
+using CalendarT1.Services.DataOperations.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CalendarT1.ViewModels.EventsViewModels
 {
-    class MonthlyEventsViewModel
-    {
-    }
+	public class MonthlyEventsViewModel : AbstractEventViewModel
+	{
+
+		public MonthlyEventsViewModel
+						(IEventRepository eventRepository)
+						: base(eventRepository) { }
+		public async override Task BindDataToScheduleList()
+		{
+			var selectedPriorities = EventPriorities.Where(x => x.IsSelected).Select(x => x.PriorityLevel).ToList();
+
+			// Start of the month
+			var startOfMonth = new DateTime(CurrentSelectedDate.Year, CurrentSelectedDate.Month, 1);
+
+			// End of the month
+			var endOfMonth = startOfMonth.AddMonths(1).AddDays(-1);
+
+			var filteredScheduleList = AllEventsList
+				.Where(x => x.StartDateTime.Date >= startOfMonth.Date &&
+							x.EndDateTime.Date <= endOfMonth.Date &&
+							selectedPriorities.Contains(x.PriorityLevel.PriorityLevel))
+				.ToList();
+
+			// Initialize MonthlyEvents
+			EventsToShowList = new ObservableCollection<EventModel>(filteredScheduleList);
+
+			OnOnEventsToShowListUpdated();
+		}
+	}
 }
