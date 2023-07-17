cde3524c30090f7dc7334bf3e77aafdf98a267de
invisloa <91217120+invisloa@users.noreply.github.com>
2023-07-17 21:06:46 +0200

    working monthly view (ugly view)

---
+++ CalendarT1/Views/ViewMonthlyEvents.xaml.cs
@@ -0,0 +1,31 @@
+using CalendarT1.ViewModels.EventsViewModels;
+using CalendarT1.Services;
+
+
+namespace CalendarT1.Views;
+
+public partial class ViewMonthlyEvents : ContentPage
+{
+	public ViewMonthlyEvents()
+	{
+		InitializeComponent();
+		var viewModel = new MonthlyEventsViewModel(Factory.EventRepository);
+		BindingContext = viewModel;
+		viewModel.OnEventsToShowListUpdated += () =>
+		{
+			monthlyEventsControl.GenerateGrid();
+		};
+	}
+	protected override void OnDisappearing()
+	{
+		base.OnDisappearing();
+		(BindingContext as MonthlyEventsViewModel).OnEventsToShowListUpdated -= monthlyEventsControl.GenerateGrid;
+	}
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		await (BindingContext as MonthlyEventsViewModel).LoadAndBindDataToScheduleListAsync();
+	}
+}
+
+
