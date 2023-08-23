0e2812be049a2e2d73987b598dd056d27558f51b
invisloa <91217120+invisloa@users.noreply.github.com>
2023-08-23 23:09:36 +0200

    Normal Label Watermark Date

--- CalendarT1/Views/CustomControls/MonthlyEventsControl.cs
+++ CalendarT1/Views/CustomControls/MonthlyEventsControl.cs
@@ -1,6 +1,8 @@
 ﻿namespace CalendarT1.Views.CustomControls
 {
+	using Microsoft.Maui.Graphics;
 	using CalendarT1.Models.EventModels;
+	using Microsoft.Maui.Layouts;
 	using System;
     using System.Collections.ObjectModel;
     using System.Linq;
@@ -9,6 +11,14 @@
     public class MonthlyEventsControl : MauiGrid
 	{
 		private int _displayEventsLimit = 2;
+		private int _watermarkDateTextFontSize = 20;
+		private int _eventNamesFontSize = 15;
+		private int _dayNamesFontSize = 15;
+
+		
+		private Color _watermarkDateColor = Color.FromArgb("#FFFFFF");	//gray color
+
+
 		public static readonly BindableProperty CurrentSelectedDateProperty =
 			BindableProperty.Create(
 				nameof(CurrentSelectedDate),
@@ -116,7 +126,7 @@
 			string[] dayLabels = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 			for (int day = 0; day < 7; day++)
 			{
-				var dayLabel = new Label { FontSize = 12, FontAttributes = FontAttributes.Bold, Text = dayLabels[day] };
+				var dayLabel = new Label { FontSize = _dayNamesFontSize, FontAttributes = FontAttributes.Bold, Text = dayLabels[day] };
 				Grid.SetRow(dayLabel, 0);
 				Grid.SetColumn(dayLabel, day);
 				Children.Add(dayLabel);
@@ -125,73 +135,82 @@
 			// Add cells for each day
 			for (int day = 1; day <= daysInMonth; day++)
 			{
+				var stackLayoutForFrame = new StackLayout();
+
 				int gridRow = (firstDayOfWeek + day - 1) / 7 + 1;
 				int gridColumn = (firstDayOfWeek + day - 1) % 7;
 
 				var frame = new Frame { BorderColor = Color.FromRgba(255, 255, 255, 255), Padding = 5, MinimumWidthRequest = 100, MinimumHeightRequest = 50 };
 				frame.AutomationId = new DateTime(CurrentSelectedDate.Year, CurrentSelectedDate.Month, day).ToString("yyyy-MM-dd");
 
+
+				var dayLabel = new Label    // add Day number watermark
+				{
+					Text = day.ToString(),
+					FontSize = _watermarkDateTextFontSize,
+					FontAttributes = FontAttributes.Bold,
+					TextColor = _watermarkDateColor,
+					VerticalOptions = LayoutOptions.Start,
+					HorizontalOptions = LayoutOptions.End
+				};
+				stackLayoutForFrame.Children.Add(dayLabel);
+
 				var dayEvents = EventsToShowList
 					.Where(e => e.StartDateTime.Date == new DateTime(CurrentSelectedDate.Year, CurrentSelectedDate.Month, day))
 					.ToList();
-				
+
 				// if there are events on this day, display them
 				if (dayEvents != null && dayEvents.Count > 0)
 				{
 					var eventColor = dayEvents[0].EventVisibleColor;
 					frame.BackgroundColor = eventColor;
-					// Create a StackLayout for the events
-					var stackLayout = new StackLayout();
 
-					  // Set a limit to how many items will be displayed
+					// Set a limit to how many items will be displayed
 					for (int i = 0; i < Math.Min(dayEvents.Count, _displayEventsLimit); i++)
 					{
 						var label = new Label
 						{
-							FontSize = 15,
-							HorizontalTextAlignment = TextAlignment.Center,
+							FontSize = _eventNamesFontSize,
 							FontAttributes = FontAttributes.Bold,
 							Text = dayEvents[i].Title,
 							BackgroundColor = dayEvents[i].EventVisibleColor
 						};
-						var tapGestureRecognizer = new TapGestureRecognizer();
-						tapGestureRecognizer.Command = EventSelectedCommand;
-						tapGestureRecognizer.CommandParameter = dayEvents[i];
-						label.GestureRecognizers.Add(tapGestureRecognizer);
-						stackLayout.Children.Add(label);
+
+						stackLayoutForFrame.Children.Add(label);
 					}
 					// If there are more items than the limit, add a 'See more' label
 					if (dayEvents.Count > _displayEventsLimit)
 					{
 						var moreLabel = new Label
 						{
-							FontSize = 20,
+							FontSize = _eventNamesFontSize,
 							FontAttributes = FontAttributes.Italic,
 							Text = $"... {dayEvents.Count - _displayEventsLimit} MORE ...",
 							HorizontalTextAlignment = TextAlignment.Center,
 							TextColor = Color.FromRgba(255, 255, 255, 255),
 							BackgroundColor = Color.FromRgba(0, 0, 0, 100)
 						};
-						var tapGestureRecognizerForMoreEvents = new TapGestureRecognizer();
-						tapGestureRecognizerForMoreEvents.Command = GoToSelectedDateCommand;
-						tapGestureRecognizerForMoreEvents.CommandParameter = DateTime.Parse(frame.AutomationId);
 
-
-						frame.GestureRecognizers.Add(tapGestureRecognizerForMoreEvents);
-						moreLabel.GestureRecognizers.Add(tapGestureRecognizerForMoreEvents);
-						stackLayout.Children.Add(moreLabel);
+						stackLayoutForFrame.Children.Add(moreLabel);
 					}
 
-					frame.Content = stackLayout;
+					// one tap if there are any events
+					var oneTapGestureRecognizerForMoreEvents = new TapGestureRecognizer();
+					oneTapGestureRecognizerForMoreEvents.Command = GoToSelectedDateCommand;
+					oneTapGestureRecognizerForMoreEvents.CommandParameter = DateTime.Parse(frame.AutomationId);
+					frame.GestureRecognizers.Add(oneTapGestureRecognizerForMoreEvents);
 				}
-				else //There are no events in a specific day
+				else
 				{
+					// double tap if there are no event to go to the selected date
 					var tapGestureRecognizerForMoreEvents = new TapGestureRecognizer();
+					tapGestureRecognizerForMoreEvents.NumberOfTapsRequired = 2;
 					tapGestureRecognizerForMoreEvents.Command = GoToSelectedDateCommand;
 					tapGestureRecognizerForMoreEvents.CommandParameter = DateTime.Parse(frame.AutomationId);
 					frame.GestureRecognizers.Add(tapGestureRecognizerForMoreEvents);
 				}
 
+				frame.Content = stackLayoutForFrame;
 				Grid.SetRow(frame, gridRow);
 				Grid.SetColumn(frame, gridColumn);
 				Children.Add(frame);
