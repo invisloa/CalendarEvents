41431be074d685fbde85f6f457d69bdd668d1c23
invisloa <91217120+invisloa@users.noreply.github.com>
2023-09-26 21:50:18 +0200

    ...

--- CalendarT1/Views/CustomControls/EventsPagesCC/BaseEventPageCC.cs
+++ CalendarT1/Views/CustomControls/EventsPagesCC/BaseEventPageCC.cs
@@ -8,66 +8,67 @@ namespace CalendarT1.Views.CustomControls
 	using System.Linq;
 	using MauiGrid = Microsoft.Maui.Controls.Grid;
 
-public class BaseEventPageCC : MauiGrid
-{
-
-	public static readonly BindableProperty CurrentSelectedDateProperty =
-		 BindableProperty.Create(
-			 nameof(CurrentSelectedDate),
-			 typeof(DateTime),
-			 typeof(BaseEventPageCC),
-			 defaultBindingMode: BindingMode.TwoWay);
-	public DateTime CurrentSelectedDate
+	public class BaseEventPageCC : MauiGrid
 	{
-		get => (DateTime)GetValue(CurrentSelectedDateProperty);
-		set => SetValue(CurrentSelectedDateProperty, value);
-	}
 
-	public static readonly BindableProperty EventsToShowListProperty =
-		BindableProperty.Create(
-		nameof(EventsToShowList),
-		typeof(ObservableCollection<IGeneralEventModel>),
-		typeof(BaseEventPageCC),
-		defaultBindingMode: BindingMode.TwoWay);
-	public ObservableCollection<IGeneralEventModel> EventsToShowList
-	{
-		get => (ObservableCollection<IGeneralEventModel>)GetValue(EventsToShowListProperty);
-		set => SetValue(EventsToShowListProperty, value);
-	}
+		public static readonly BindableProperty CurrentSelectedDateProperty =
+			 BindableProperty.Create(
+				 nameof(CurrentSelectedDate),
+				 typeof(DateTime),
+				 typeof(BaseEventPageCC),
+				 defaultBindingMode: BindingMode.TwoWay);
+		public DateTime CurrentSelectedDate
+		{
+			get => (DateTime)GetValue(CurrentSelectedDateProperty);
+			set => SetValue(CurrentSelectedDateProperty, value);
+		}
 
-	public static readonly BindableProperty AllEventsListProperty =
-		BindableProperty.Create(
-		nameof(AllEventsList),
-		typeof(ObservableCollection<IGeneralEventModel>),
-		typeof(BaseEventPageCC));
+		public static readonly BindableProperty EventsToShowListProperty =
+			BindableProperty.Create(
+			nameof(EventsToShowList),
+			typeof(ObservableCollection<IGeneralEventModel>),
+			typeof(BaseEventPageCC),
+			defaultBindingMode: BindingMode.TwoWay);
+		public ObservableCollection<IGeneralEventModel> EventsToShowList
+		{
+			get => (ObservableCollection<IGeneralEventModel>)GetValue(EventsToShowListProperty);
+			set => SetValue(EventsToShowListProperty, value);
+		}
 
-	public ObservableCollection<IGeneralEventModel> AllEventsList
-	{
-		get => (ObservableCollection<IGeneralEventModel>)GetValue(AllEventsListProperty);
-		set => SetValue(AllEventsListProperty, value);
-	}
+		public static readonly BindableProperty AllEventsListProperty =
+			BindableProperty.Create(
+			nameof(AllEventsList),
+			typeof(ObservableCollection<IGeneralEventModel>),
+			typeof(BaseEventPageCC));
 
-	public static readonly BindableProperty EventSelectedCommandProperty =
-		BindableProperty.Create(
-		nameof(EventSelectedCommand),
-		typeof(RelayCommand<IGeneralEventModel>),
-		typeof(BaseEventPageCC));
+		public ObservableCollection<IGeneralEventModel> AllEventsList
+		{
+			get => (ObservableCollection<IGeneralEventModel>)GetValue(AllEventsListProperty);
+			set => SetValue(AllEventsListProperty, value);
+		}
 
-	public RelayCommand<IGeneralEventModel> EventSelectedCommand
-	{
-		get => (RelayCommand<IGeneralEventModel>)GetValue(EventSelectedCommandProperty);
-		set => SetValue(EventSelectedCommandProperty, value);
-	}
+		public static readonly BindableProperty EventSelectedCommandProperty =
+			BindableProperty.Create(
+			nameof(EventSelectedCommand),
+			typeof(RelayCommand<IGeneralEventModel>),
+			typeof(BaseEventPageCC));
 
-	public static readonly BindableProperty GoToSelectedDateCommandProperty =
-		BindableProperty.Create(
-		nameof(GoToSelectedDateCommand),
-		typeof(RelayCommand<DateTime>),
-		typeof(BaseEventPageCC));
+		public RelayCommand<IGeneralEventModel> EventSelectedCommand
+		{
+			get => (RelayCommand<IGeneralEventModel>)GetValue(EventSelectedCommandProperty);
+			set => SetValue(EventSelectedCommandProperty, value);
+		}
 
-	public RelayCommand<DateTime> GoToSelectedDateCommand
-	{
-		get => (RelayCommand<DateTime>)GetValue(GoToSelectedDateCommandProperty);
-		set => SetValue(GoToSelectedDateCommandProperty, value);
+		public static readonly BindableProperty GoToSelectedDateCommandProperty =
+			BindableProperty.Create(
+			nameof(GoToSelectedDateCommand),
+			typeof(RelayCommand<DateTime>),
+			typeof(BaseEventPageCC));
+
+		public RelayCommand<DateTime> GoToSelectedDateCommand
+		{
+			get => (RelayCommand<DateTime>)GetValue(GoToSelectedDateCommandProperty);
+			set => SetValue(GoToSelectedDateCommandProperty, value);
+		}
 	}
 }
