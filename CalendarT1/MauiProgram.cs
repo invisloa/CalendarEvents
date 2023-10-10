c5c8d92971fba28455010bd1ef63529baf16d646
invisloa <91217120+invisloa@users.noreply.github.com>
2023-10-10 23:26:24 +0200

    google iconfont added

--- CalendarT1/MauiProgram.cs
+++ CalendarT1/MauiProgram.cs
@@ -28,7 +28,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("MaterialIcons-Regular.ttf", "GoogleFonts");
+			}); 
 
 		// Interfaces DI Dependency Injection for events repository
 		builder.Services.AddSingleton<IEventRepository, LocalMachineEventRepository>();			// events repository DI
