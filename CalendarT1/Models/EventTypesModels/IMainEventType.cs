1210fbaf285b95dcfaab79866a93d2ffa01f1d29
invisloa <91217120+invisloa@users.noreply.github.com>
2023-10-12 23:46:05 +0200

    maineventtype equals update

--- CalendarT1/Models/EventTypesModels/IMainEventType.cs
+++ CalendarT1/Models/EventTypesModels/IMainEventType.cs
@@ -9,7 +9,8 @@ namespace CalendarT1.Models.EventTypesModels
 	public interface IMainEventType : IEquatable<object>
 	{
 		string Title { get; set; }
-		Color MainEventTypeBackgroundColor { get; set; } 
+		string Icon { get; set; }
+		Color MainEventTypeBackgroundColor { get; set; }
 		Color MainEventTypeTextColor { get; set; }
 		new bool Equals(object other); // to check if the event type is already in the list
 	}
--- CalendarT1/Models/EventTypesModels/MainEventType.cs
+++ CalendarT1/Models/EventTypesModels/MainEventType.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,63 +12,43 @@ namespace CalendarT1.Models.EventTypesModels
 	public class MainEventType : IMainEventType
 	{
 		public string Title { get; set; }
-		private Color _mainEventTypeBackgroundColor;
+		public string Icon { get; set; }  // new property for the icon
 
+		private Color _mainEventTypeBackgroundColor;
 		[JsonIgnore]
 		public Color MainEventTypeBackgroundColor
 		{
 			get => _mainEventTypeBackgroundColor;
-			set
-			{
-				if (_mainEventTypeBackgroundColor != value)
-				{
-					_mainEventTypeBackgroundColor = value;
-				}
-			}
+			set => _mainEventTypeBackgroundColor = value; 
 		}
-		public string MainEventTypeBackgroundColorString       
+
+		public string MainEventTypeBackgroundColorString
 		{
-			get
-			{
-				return _mainEventTypeBackgroundColor.ToArgbHex();
-			}
-			set
-			{
-				_mainEventTypeBackgroundColor = Color.FromArgb(value);
-			}
+			get => _mainEventTypeBackgroundColor.ToArgbHex();
+			set => _mainEventTypeBackgroundColor = Color.FromArgb(value); 
 		}
+
 		private Color _mainEventTypeTextColor;
 		[JsonIgnore]
-		public Color MainEventTypeTextColor 
-		{ get => _mainEventTypeTextColor;
-			set
-			{
-				if(_mainEventTypeTextColor != value)
-				{
-					_mainEventTypeTextColor = value;
-				}
-			}
+		public Color MainEventTypeTextColor
+		{
+			get => _mainEventTypeTextColor;
+			set => _mainEventTypeTextColor = value; 
 		}
 
 		public string MainEventTypeTextColorString
 		{
-			get
-			{
-				return _mainEventTypeTextColor.ToArgbHex();
-			}
-
-			set
-			{
-				_mainEventTypeTextColor = Color.FromArgb(value);
-			}
+			get => _mainEventTypeTextColor.ToArgbHex(); 
+			set => _mainEventTypeTextColor = Color.FromArgb(value); 
 		}
 		public override string ToString()
 		{
 			return Title;
 		}
-		public MainEventType(string title, Color mainEventTypeBackgroundColor, Color mainEventTypeTextColor)
+		public MainEventType(string title, string icon, Color mainEventTypeBackgroundColor, Color mainEventTypeTextColor)
 		{
 			Title = title;
+			Icon = icon; 
 			MainEventTypeBackgroundColor = mainEventTypeBackgroundColor;
 			MainEventTypeTextColor = mainEventTypeTextColor;
 		}
@@ -80,13 +61,12 @@ namespace CalendarT1.Models.EventTypesModels
 			}
 
 			MainEventType other = (MainEventType)obj;
-			return Title == other.Title;
+			return Title == other.Title && Icon == other.Icon && MainEventTypeBackgroundColorString == other.MainEventTypeBackgroundColorString && MainEventTypeTextColor == other.MainEventTypeTextColor ;
 		}
 
 		public override int GetHashCode()
 		{
-			return Title.GetHashCode();
+			return (Title, Icon, MainEventTypeBackgroundColorString, MainEventTypeTextColor).GetHashCode();
 		}
-
 	}
 }
