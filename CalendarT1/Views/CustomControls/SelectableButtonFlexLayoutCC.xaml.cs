7376728ac1bed154d69ebc1ceef26d4904cef63c
invisloa <91217120+invisloa@users.noreply.github.com>
2023-10-15 00:25:07 +0200

    <customControls:SelectableButtonFlexLayoutCC ItemsSource="{Binding MainButtonVisualsSelectors}" />

---
+++ CalendarT1/Views/CustomControls/SelectableButtonFlexLayoutCC.xaml.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+
+namespace CalendarT1.Views.CustomControls;
+
+public partial class SelectableButtonFlexLayoutCC : FlexLayout
+{
+	public SelectableButtonFlexLayoutCC()
+	{
+		InitializeComponent();
+	}
+
+	public static readonly BindableProperty ItemsSourceProperty =
+		BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable), typeof(SelectableButtonFlexLayoutCC), null, propertyChanged: OnItemsSourceChanged);
+
+	public IEnumerable ItemsSource
+	{
+		get => (IEnumerable)GetValue(ItemsSourceProperty);
+		set => SetValue(ItemsSourceProperty, value);
+	}
+
+
+	private static void OnItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is SelectableButtonFlexLayoutCC control && newValue is IEnumerable items)
+		{
+			BindableLayout.SetItemsSource(control, items);
+		}
+	}
+}
