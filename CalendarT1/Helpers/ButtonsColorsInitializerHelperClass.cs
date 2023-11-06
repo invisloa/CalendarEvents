a57f77b24ec415d1b5a2d503d6346ad8f0685698
invisloa <91217120+invisloa@users.noreply.github.com>
2023-11-06 23:52:16 +0100

    Colors Pallete Update

--- CalendarT1/Helpers/ButtonsColorsInitializerHelperClass.cs
+++ CalendarT1/Helpers/ButtonsColorsInitializerHelperClass.cs
@@ -23,38 +23,68 @@ namespace CalendarT1.Helpers
 		{
 			ButtonsColorsOC = new ObservableCollection<SelectableButtonViewModel>();
 
-			AddColors(GenerateShades(Color.FromRgb(205, 92, 92))); // Base Red
-			AddColors(GenerateShades(Color.FromRgb(30, 144, 255))); // Base Blue
-			AddColors(GenerateShades(Color.FromRgb(60, 179, 113))); // Base Green
-			AddColors(GenerateShades(Color.FromRgb(255, 165, 0))); // Base Orange
-			// ... Add more base colors as needed
+			// Define base color groups
+			var colorGroups = new List<Color[]>
+			{
+				new Color[] { Color.FromRgb(255, 0, 0), Color.FromRgb(205, 92, 92) }, // Reds
+				new Color[] { Color.FromRgb(0, 128, 0), Color.FromRgb(60, 179, 113) }, // Greens
+				new Color[] { Color.FromRgb(0, 0, 255), Color.FromRgb(30, 144, 255) }, // Blues
+				new Color[] { Color.FromRgb(255, 165, 0), Color.FromRgb(255, 215, 0) }, // Oranges & Yellows
+				// Add more color groups as needed, with each group containing similar colors
+			};
+
+			// Randomize the order of the color groups to mix the palette
+			var rnd = new Random();
+			var randomizedGroups = colorGroups.OrderBy(x => rnd.Next()).ToList();
+
+			// For each group, generate and add shades
+			foreach (var group in randomizedGroups)
+			{
+				foreach (var baseColor in group)
+				{
+					AddColors(GenerateShades(baseColor));
+				}
+			}
 		}
 
 		private void AddColors(params Color[] colors)
 		{
 			foreach (var color in colors)
 			{
-				ButtonsColorsOC.Add(new SelectableButtonViewModel { ButtonColor = color});
+				ButtonsColorsOC.Add(new SelectableButtonViewModel { ButtonColor = color });
 			}
 		}
 
 		private Color[] GenerateShades(Color baseColor)
 		{
 			Color[] shades = new Color[NumberOfColumns];
-			double step = 0.1;
+			double lightnessRange = 0.5; // Range for lightening (e.g., from 1.0 for base color to 1.5 for lightest color)
+			double darknessRange = 0.5; // Range for darkening (e.g., from 1.0 for base color to 0.5 for darkest color)
+
+			// Calculate the step for each shade based on the number of columns
+			double step = (lightnessRange + darknessRange) / (NumberOfColumns - 1);
 
 			for (int i = 0; i < NumberOfColumns; i++)
 			{
-				double factor = 0.7 + step * i; // Start from 70% brightness and increment
+				// Calculate factor for current shade
+				double factor = 1 + lightnessRange - step * i;
+
+				// Clamp factor to not exceed the range of [0,1] after adjustment
+				double red = Math.Max(0, Math.Min(baseColor.Red * factor, 1.0));
+				double green = Math.Max(0, Math.Min(baseColor.Green * factor, 1.0));
+				double blue = Math.Max(0, Math.Min(baseColor.Blue * factor, 1.0));
+
 				shades[i] = Color.FromRgba(
-					baseColor.Red * factor,
-					baseColor.Green * factor,
-					baseColor.Blue * factor,
+					red,
+					green,
+					blue,
 					1 // Alpha for opacity
 				);
 			}
 
 			return shades;
 		}
+
+
 	}
 }
